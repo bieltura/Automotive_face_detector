bd90b83b3be9dca21cd3ca3328904a46abb6571a
bieltv3 <bieltv.3@gmail.com>
2018-12-19 12:13:51 +0100

    Thread managment v1

--- main.py
+++ main.py
@@ -1,31 +1,32 @@
 import cv2
+import threading
+import sys
 
 from obj import devices
 from utils import haar_face_detection as fd
 
 # Cameras (to be written into a file)
-num_cameras = 1
+num_cameras = 2
 
 # Face size (square in px for CNN)
 face_size = 250
 
 # Number of cameras in the system
 camera_thread = []
-cameras = []
 
-for cam in range(num_cameras):
-    cameras.append(devices.Camera(cam))
+def run_camera(cam_id):
 
-while True:
+    cam = devices.Camera(cam_id)
 
-    captures = []
+    while True:
 
-    for cam_id, cam in enumerate(cameras):
+        captures = []
+
         frame = cam.getFrame()
 
         frame_monitor = cv2.resize(frame, (480, 320))
 
-        cv2.imshow('Camera ' + str(cam_id), frame_monitor)
+        #cv2.imshow('Camera ' + str(cam_id), frame_monitor)
 
         captures.append(frame_monitor)
 
@@ -33,15 +34,23 @@ while True:
 
         if face is not -1:
             # Neural Net
-            cv2.imshow('Camera ' + str(cam_id), face)
+            print("face detected in camera" + str(cam_id))
+            #cv2.imshow('Camera ' + str(cam_id), face)
 
+    # When everything done, release the capture
+    cam.close()
+    #cv2.destroyAllWindows()
+
+
+for cam in range(num_cameras):
+    cam_thread = threading.Thread(target=run_camera, args=(cam,))
+    camera_thread.append(cam_thread)
+    cam_thread.start()
+
+while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
-    monitor = cv2.hconcat(captures)
+        # Kill threads?
+        #for thread in cam_thread:
 
-    # cv2.imshow('Monitor', monitor)
-
-# When everything done, release the capture
-cam.close()
-cv2.destroyAllWindows()
+        sys.exit(0)
